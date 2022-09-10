3fe52515a289b908f0aed3576e8c88cfc4b8d49b
Mitchell Ergen <mitchellergen@gmail.com>
2022-09-10 07:03:59 -0500

    + ReservationTest.java

---
+++ project/src/test/java/se452/group4/project/ReservationTest.java
@@ -0,0 +1,36 @@
+package se452.group4.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+import se452.group4.project.reservation.Reservation;
+
+public class ReservationTest {
+    
+
+    @Test
+    public void testToString() {
+        var r = new Reservation();
+        var id = UUID.randomUUID();
+        var showTimeId = UUID.randomUUID();
+        var studentId = UUID.randomUUID();
+        var startTime = LocalDateTime.now(); 
+        var description = "Some important Description of a reservation for a movie.";
+
+        var expectedResult = MessageFormat.format(
+            "Reservation(id={0}, description={1}, startTime={2}, showTimeId={3}, studentId={4})", 
+            id, description, startTime, showTimeId, studentId);
+
+        r.id = id;
+		r.showTimeId = showTimeId;
+		r.studentId = studentId;
+		r.startTime = startTime;
+		r.description = description;
+		assertEquals(expectedResult, r.toString());
+    }
+}
