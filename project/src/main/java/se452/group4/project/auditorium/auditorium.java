3a2887bad8d01846baaa2c1a5ab3536e59dfdab7
umme-e-hani97 <99371513+umme-e-hani97@users.noreply.github.com>
2022-09-13 15:21:37 -0500

    Changes

--- project/src/main/java/se452/group4/project/auditorium/auditorium.java
+++ project/src/main/java/se452/group4/project/auditorium/auditorium.java
@@ -2,6 +2,11 @@ package se452.group4.project.auditorium;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
 import se452.group4.project.entity.Entity;
 
 public class auditorium extends Entity{
