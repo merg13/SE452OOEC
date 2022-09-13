b731f9bd44530653c9027486cf14ff52a0374e82
Tan Nguyen <dev.tannguyen@gmail.com>
2022-09-13 00:32:47 -0500

    Fix typo for test name (#10)

--- project/src/test/java/se452/group4/project/admin/AdminTest.java
+++ project/src/test/java/se452/group4/project/admin/AdminTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AdminTest {  
-    @DisplayName("Test Customer")
+    @DisplayName("Test Admin")
     @Test
     public void testAdmin() {
         Admin admin = new Admin();
