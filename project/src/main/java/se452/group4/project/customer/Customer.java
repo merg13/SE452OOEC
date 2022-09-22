042cface5e3980eea98ffd8f8def6b0c5f7e99d8
Tan Nguyen <dev.tannguyen@gmail.com>
2022-09-22 17:57:41 -0500

    Add Customer and Admin Repo

--- project/src/main/java/se452/group4/project/customer/Customer.java
+++ project/src/main/java/se452/group4/project/customer/Customer.java
@@ -5,13 +5,17 @@ import lombok.EqualsAndHashCode;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
+import javax.persistence.Table;
 
 import se452.group4.project.entity.Entity;
 
+
 @Data
+@javax.persistence.Entity
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
+@Table(name = "customer")
 public class Customer extends Entity {
     @NonNull
     public String name;
