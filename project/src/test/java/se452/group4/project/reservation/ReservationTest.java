7bf40b6c20d7a0a4c80a59c8c634011e9563f1be
Mitchell Ergen <mitchellergen@gmail.com>
2022-09-23 21:03:41 -0500

    bad merge fix.

--- project/src/test/java/se452/group4/project/reservation/ReservationTest.java
+++ project/src/test/java/se452/group4/project/reservation/ReservationTest.java
@@ -9,31 +9,13 @@ import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 @SpringBootTest
 public class ReservationTest {
     
     // @Autowired uses Spring to instantiate an instance of the ReservationRepository for use in unit testing. 
     @Autowired
     private ReservationRepo repo;
-
-    /**
-     * TEST: ReservationRepository Extending JPARepository - CRUD
-     */
-    @Test
-    public void testReservationRepositoryCRUD() {
-        // ARRANGE
-        var r = createReservation();
-
-        // ACT
-        repo.save(r);
-
-        // ASSERT - Assertion Includes seed data from data.sql.
-        assertEquals(2, repo.count());
-    }
 
     /**
      * TEST: Reservation Object ToString w/ LOMBOK
@@ -65,18 +47,6 @@ public class ReservationTest {
             r.getId(), r.getCreated(), r.getDescription(), r.getStartTime(), r.getShowTimeId(), r.getCustomerId());
             
 		assertEquals(expectedResult, r.toString());
-    }
-
-    private Reservation createReservation() {
-        var r = new Reservation();
-        r.setId(UUID.randomUUID());
-        r.setCreated(LocalDateTime.now());
-        r.setDescription("UnitTestDescription");
-        r.setStartTime(LocalDateTime.now().plusHours(5));
-        r.setCustomerId(UUID.randomUUID());
-        r.setShowTimeId(UUID.randomUUID());
-
-        return r;
     }
 
     private Reservation createReservation() {
