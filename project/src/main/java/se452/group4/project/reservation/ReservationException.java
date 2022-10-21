ccb06d8b217f0f509355f16348894ae1be285386
Mitchell Ergen <mitchellergen@gmail.com>
2022-10-21 12:05:50 -0500

    error messages for reservatione exception.

--- project/src/main/java/se452/group4/project/reservation/ReservationException.java
+++ project/src/main/java/se452/group4/project/reservation/ReservationException.java
@@ -5,9 +5,9 @@ import java.util.UUID;
 public class ReservationException extends Exception {
     public static String GetAllReservationsError = "ERROR: GetAllReservations: could not successfully get all reservations.";
     public static String GetReservationByIdError = "ERROR: GetReservationById: could not successfully get ReservationById: ";
-    public static String UpsertReservationError = "";
-    public static String CreateReservationError = "";
-    public static String DeleteReservationByIdError = "";
+    public static String UpsertReservationError = "ERROR: UpsertReservation: could not successfully upsert ReservationById: ";
+    public static String CreateReservationError = "ERROR: CreateReservarion: could not successfully create Reservation.";
+    public static String DeleteReservationByIdError = "ERROR: DeleteReservationById: could not successfully delete ReservationById: ";
 
     public ReservationException(String errorMessage, Throwable cause) {
         super(errorMessage, cause);
