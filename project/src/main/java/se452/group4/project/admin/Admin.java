684ad936490ebbd28732d88c39d5e7aa74cf6b31
Eric Margules <eric.i.margules@gmail.com>
2022-11-08 17:46:59 -0600

    Add missing callToSuper for child entities (#36)

--- project/src/main/java/se452/group4/project/admin/Admin.java
+++ project/src/main/java/se452/group4/project/admin/Admin.java
@@ -1,12 +1,16 @@
 package se452.group4.project.admin;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import org.springframework.data.redis.core.RedisHash;
 
 import se452.group4.project.entity.Entity;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 @RedisHash("Admin")
 public class Admin extends Entity {
 }
