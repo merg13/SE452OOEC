a4294b2398de0988dd326e9b739aa0d9c37dcc6c
Tan Nguyen <dev.tannguyen@gmail.com>
2022-11-08 15:53:12 -0600

    Fix Auditorium Redis NoSQL

--- project/src/main/java/se452/group4/project/auditorium/Auditorium.java
+++ project/src/main/java/se452/group4/project/auditorium/Auditorium.java
@@ -5,15 +5,19 @@ import lombok.Data;
 import se452.group4.project.entity.Entity;
 
 import org.springframework.data.redis.core.RedisHash;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
-@RedisHash("Admin")
-
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@RedisHash("Auditorium")
 public class Auditorium extends Entity {
     
     public String name;
     public int capacity;
-
-    public Auditorium() {}
-
 }
--- project/src/main/java/se452/group4/project/auditorium/AuditoriumException.java
+++ project/src/main/java/se452/group4/project/auditorium/AuditoriumException.java
@@ -8,9 +8,7 @@ public class AuditoriumException extends Exception {
     public static String GetAllCapacity = "ERROR: GetAllCapacity: could not successfully get all CapacityId.";
     public static String GetShowtimeByIdError = "ERROR: GetShowtimeByIdError: could not successfully get all showtimeId.";
     public static String GetCapacityByIdError = "ERROR: GetCapacityByIdError: could not successfully get all showtimeId.";
-
-
-
+    public static String CreateAuditoriumError = "ERROR: CreateAuditoriumError: could not successfully create Auditorium.";
 
     public AuditoriumException(String errorMessage, Throwable cause) {
         super();
